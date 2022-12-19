c81f014cd2d7cc5b3ce7e358507c414e9d145471
mirroredcube <anitha.praveenrao@gmail.com>
2022-12-19 12:12:55 +0100

    Add elements

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -6,11 +8,20 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Homepage'
-      });
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
 });
 
 const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`);
 });
